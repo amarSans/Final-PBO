066c63e0ef9a7b922dc59526f5edbe15a785c88c
amarSans <136565662+amarSans@users.noreply.github.com>
2023-06-23 09:47:36 +0800

    Delete tampilan.java

--- tampilan.java
+++
@@ -1,8 +0,0 @@
-public interface tampilan {
-    void Menang();
-    void kalah();
-    void tampilanMenu();
-    void tampilanSetting();
-     void tampilanCloseGame();
-     void tampilanNegara();
-}
